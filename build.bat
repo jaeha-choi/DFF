756f119db33cd958448da360b1efcae6cc95d7c6
Jaeha Choi <jaeha@mail.jaeha.dev>
2022-01-27 22:46:01 -0800

    Update build script to include parent folder

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ mkdir test
 set GOOS=windows
 set GOARCH=amd64
 set FYNE_FONT=C:\Windows\Fonts\Arial.ttf
-go build -o %cd%\build\DFF_win_debug.exe %cd%\cmd\dff.go
-go build -o %cd%\build\DFF_win.exe -ldflags -H=windowsgui %cd%\cmd\dff.go
-7z a %cd%\out\DFF_windows.zip %cd%\build\DFF_win.exe
-7z a %cd%\out\DFF_windows_debug.zip %cd%\build\DFF_win_debug.exe
+go build -o %cd%\build\DFF_win_debug\DFF_win_debug.exe %cd%\cmd\dff.go
+go build -o %cd%\build\DFF_win\DFF_win.exe -ldflags -H=windowsgui %cd%\cmd\dff.go
+7z a %cd%\out\DFF_windows.zip %cd%\build\DFF_win\
+7z a %cd%\out\DFF_windows_debug.zip %cd%\build\DFF_win_debug\
 pause
